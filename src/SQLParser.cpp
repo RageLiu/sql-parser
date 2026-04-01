23e894df727ab4d37a71a55814bf14748672c357
RageLiu <2273015469@qq.com>
2026-04-01 09:22:14 +0800

    Temporarily remove fix to verify sanitizer failure

--- src/SQLParser.cpp
+++ src/SQLParser.cpp
@@ -59,16 +59,11 @@ bool SQLParser::tokenize(const std::string& sql, std::vector<int16_t>* tokens) {
   int16_t token = hsql_lex(&yylval, &yylloc, scanner);
   while (token != 0) {
     tokens->push_back(token);
-  
-    if (token == SQL_IDENTIFIER || token == SQL_STRING) {
-      free(yylval.sval);
-      yylval.sval = nullptr;
-
-    }
-
     token = hsql_lex(&yylval, &yylloc, scanner);
 
-
+    if (token == SQL_IDENTIFIER || token == SQL_STRING) {
+      free(yylval.sval);
+    }
   }
 
   hsql__delete_buffer(state, scanner);
